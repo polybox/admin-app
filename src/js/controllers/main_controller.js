277d43602dc725d6dfd5b29c09bfec1f0feaa963
Marcos Lilljedahl <marcosnils@gmail.com>
2016-09-07 17:50:14 -0300

    Add back button for app route and fix main display

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -1,48 +1,60 @@
 angular.module('MobyOSAdmin.controllers.Main', [])
 
-.controller('MainController', ['$scope', '$window','$http', function($scope, $window, $http){
-    $scope.installedApps = [];
-
-    $http.get('/apps').then(function(response) {
-      response.data.forEach(function(app) {
-        $scope.installedApps.push(app);
-            //{
-                //name: 'VLC media player',
-                //description: 'VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols.',
-                //running: true,
-                //icon: 'http://i.utdstc.com/icons/256/vlc-media-player-1-0-5.png',
-                //admin_location: 'http://127.0.0.1:30001'
-            //},
+.directive('back', ['$window', function($window) {
+  return {
+    restrict: 'A',
+    link: function (scope, elem, attrs) {
+      elem.bind('click', function () {
+        $window.history.back();
       });
-    }, function(response) {
-    });
+    }
+  };
 }])
+
+.controller('MainController', ['$scope', '$window','$http', function($scope, $window, $http){
+  $scope.installedApps = [];
+
+  $http.get('/apps').then(function(response) {
+    response.data.forEach(function(app) {
+      $scope.installedApps.push(app);
+      //{
+      //name: 'VLC media player',
+      //description: 'VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols.',
+      //running: true,
+      //icon: 'http://i.utdstc.com/icons/256/vlc-media-player-1-0-5.png',
+      //admin_location: 'http://127.0.0.1:30001'
+      //},
+    });
+  }, function(response) {
+  });
+}])
+
 .controller('AppController', ['$scope', '$window','$http','$routeParams', function($scope, $window, $http, $routeParams){
-    $scope.app = null;
+  $scope.app = null;
 
-    var appId = $routeParams.id;
+  var appId = $routeParams.id;
 
-    $http.get('/apps/' + appId).then(function(response) {
-      $scope.app = response.data;
+  $http.get('/apps/' + appId).then(function(response) {
+    $scope.app = response.data;
+  }, function(response) {
+  });
+
+  $scope.runApp = function(app) {
+    $http.post('/apps/' + app.id + '/start').then(function(response) {
+      $scope.app.is_running = true;
     }, function(response) {
     });
+  }
 
-    $scope.runApp = function(app) {
-	    $http.post('/apps/' + app.id + '/start').then(function(response) {
-	      $scope.app.is_running = true;
-	    }, function(response) {
-	    });
-    }
+  $scope.stopApp = function(app) {
+    $http.post('/apps/' + app.id + '/stop').then(function(response) {
+      $scope.app.is_running = false;
+    }, function(response) {
+    });
+  }
 
-    $scope.stopApp = function(app) {
-	    $http.post('/apps/' + app.id + '/stop').then(function(response) {
-	      $scope.app.is_running = false;
-	    }, function(response) {
-	    });
-    }
-
-    $scope.openRemote = function(app) {
-	    $window.open(app.remote_url);
-    }
+  $scope.openRemote = function(app) {
+    $window.open(app.remote_url);
+  }
 
 }]);
