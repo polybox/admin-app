6bab14ca838aad0c1b1298acf0a27cf334f94b56
Marcos Lilljedahl <marcosnils@gmail.com>
2016-09-09 04:15:59 -0300

    Add profiles section

--- src/js/app.js
+++ src/js/app.js
@@ -9,5 +9,6 @@ angular.module('MobyOSAdmin', [
   $routeProvider.when('/', {templateUrl:'apps.html',  reloadOnSearch: false});
   $routeProvider.when('/apps/:id', {templateUrl:'app.html',  controller: 'AppController', reloadOnSearch: false});
   $routeProvider.when('/store', {templateUrl:'store.html',  controller: 'StoreController', reloadOnSearch: false});
+  $routeProvider.when('/profiles', {templateUrl:'profiles.html',  controller: 'ProfileController', reloadOnSearch: false});
   $routeProvider.when('/prefs', {templateUrl:'prefs.html',  controller: 'PrefsController', reloadOnSearch: false});
 });
